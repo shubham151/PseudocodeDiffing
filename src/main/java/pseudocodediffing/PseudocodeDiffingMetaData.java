e373a1995a5c6fd722fb941b886027b9bfdf511f
Kenny Chen <kennygchen@yahoo.com>
2024-05-23 12:21:23 -0700

    Added meta data class

---
+++ src/main/java/pseudocodediffing/PseudocodeDiffingMetaData.java
@@ -0,0 +1,9 @@
+package pseudocodediffing;
+
+public class PseudocodeDiffingMetaData {
+	public static final String WINDOW_GROUP = "PseudocodeDiffingPlugin";
+	public static final String ACTION_NAME = "Pseudocode Diffing";
+
+	public static final String ICON_PATH = "/path/to/icon.png";
+	public static final String FILE_CHOOSER_ICON = "/path/to/icon.png";
+}
