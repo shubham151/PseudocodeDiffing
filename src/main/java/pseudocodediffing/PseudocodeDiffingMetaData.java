3365b717ebcf3235beedecbe3c95e23dc20c451f
Kenny Chen <kennygchen@yahoo.com>
2024-05-30 13:29:15 -0700

    Added more icon

--- src/main/java/pseudocodediffing/PseudocodeDiffingMetaData.java
+++ src/main/java/pseudocodediffing/PseudocodeDiffingMetaData.java
@@ -3,7 +3,13 @@ package pseudocodediffing;
 public class PseudocodeDiffingMetaData {
 	public static final String WINDOW_GROUP = "PseudocodeDiffingPlugin";
 	public static final String ACTION_NAME = "Pseudocode Diffing";
+	public static final String SHORT_DESCRIPTION = "Plugin short description goes here.";
+	public static final String DESCRIPTION = "Plugin long description goes here.";
 
-	public static final String ICON_PATH = "/path/to/icon.png";
+	public static final String PROJECT_ICON = "/path/to/icon.png";
 	public static final String FILE_CHOOSER_ICON = "/path/to/icon.png";
+	public static final String LOADING_ICON = "/path/to/icon.png";
+	public static final String SETTINGS_ICON = "/path/to/icon.png";
+	public static final String CLOSE_ICON = "/path/to/icon.png";
+
 }
